048d2081f2a9c91ce9312d2dade39049bb9867b2
parekhkunal <parekhkunal09@gmail.com>
2022-08-19 17:24:44 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          20DCS061 KUNAL PAREKH
+        </p>
+        <p>
+          Portfolio
+        </p>
+        <a
+          className="App-link"
+          href="https://parekhkunal.github.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kunal Parekh
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
